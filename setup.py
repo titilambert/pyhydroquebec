e42babf446925991a9b9ddb8a041748ff64de171
Thibault Cohen <titilambert@gmail.com>
2017-11-21 17:21:33 -0500

    Release 2.0.0

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import sys
+
 from pip.req import parse_requirements
 from setuptools import setup
 from pip.download import PipSession
@@ -6,18 +8,30 @@ session = PipSession()
 install_reqs = parse_requirements('requirements.txt', session=session)
 test_reqs = parse_requirements('test_requirements.txt', session=session)
 
+if sys.version_info < (3,4):
+    sys.exit('Sorry, Python < 3.4 is not supported')
+
 setup(name='pyhydroquebec',
-      version='1.3.1',
+      version='2.0.0',
       description='Get your Hydro Quebec consumption (wwww.hydroquebec.com)',
       author='Thibault Cohen',
       author_email='titilambert@gmail.com',
       url='http://github.org/titilambert/pyhydroquebec',
+      package_data={'': ['LICENSE.txt']},
+      include_package_data=True,
       packages=['pyhydroquebec'],
       entry_points={
           'console_scripts': [
               'pyhydroquebec = pyhydroquebec.__main__:main'
           ]
       },
+      license='Apache 2.0',
       install_requires=[str(r.req) for r in install_reqs],
       tests_require=[str(r.req) for r in test_reqs],
+      classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ]
+
 )
