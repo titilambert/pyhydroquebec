cc0ce3e08ce944592bee298c9e109168098395e4
NMC <n@nmc.dev>
2019-08-01 13:56:56 -0400

    Docker support (#31)
    * Add docker support.

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+# Check user and password
+if [ -z "$PYHQ_USER" ] || [ -z "$PYHQ_USER" ]
+then
+    echo 'Error: No user or password. Set both environnement variables PYHQ_USER and PYHQ_PASSWORD'
+    exit 1
+fi
+
+# Output type
+if [ ! -z "$PYHQ_OUTPUT" ]
+then
+    case "$PYHQ_OUTPUT" in
+        "TEXT") 
+            PYHQ_CMD_OUTPUT=""
+        ;;
+        "JSON")
+            PYHQ_CMD_OUTPUT="-j"
+        ;;
+        "INFLUXDB")
+            PYHQ_CMD_OUTPUT="-i"
+        ;;
+        "CONTRACTS")
+            PYHQ_CMD_OUTPUT="-l"
+        ;;
+    esac
+else
+    PYHQ_CMD_OUTPUT=""
+fi
+
+# Contract
+if [ ! -z "$PYHQ_CONTRACT" ]
+then
+    PYHQ_CMD_CONTRACT="-c $PYHQ_CONTRACT"
+fi
+
+pyhydroquebec -u $PYHQ_USER -p $PYHQ_PASSWORD $PYHQ_CMD_OUTPUT $PYHQ_CMD_CONTRACT
