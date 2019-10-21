10525e5a17ce66397591cda1614ad84c61794df9
nmc <n@nmc.dev>
2019-10-21 00:40:11 -0400

    Only fetch hourly when specified.

--- pyhydroquebec/__main__.py
+++ pyhydroquebec/__main__.py
@@ -13,7 +13,7 @@ from pyhydroquebec.mqtt_daemon import MqttHydroQuebec
 from pyhydroquebec.__version__ import VERSION
 
 
-async def fetch_data(client, contract_id):
+async def fetch_data(client, contract_id, fetch_hourly=False):
     """Fetch data for basic report."""
     await client.login()
     for customer in client.customers:
@@ -29,7 +29,8 @@ async def fetch_data(client, contract_id):
             yesterday = yesterday - timedelta(days=1)
             yesterday_str = yesterday.strftime("%Y-%m-%d")
             await customer.fetch_daily_data(yesterday_str, yesterday_str)
-        await customer.fetch_hourly_data(yesterday_str)
+        if fetch_hourly:
+            await customer.fetch_hourly_data(yesterday_str)
         return customer
 
 
@@ -111,7 +112,7 @@ def main():
     elif args.dump_data:
         async_func = dump_data(client, args.contract)
     elif args.detailled_energy is False:
-        async_func = fetch_data(client, args.contract)
+        async_func = fetch_data(client, args.contract, args.hourly)
     else:
         start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
         end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
